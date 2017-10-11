92c2786f02dfa606bcdcf39b78d7ae03b1bdde2a
Jim Vickery <jimrv8@yahoo.com>
2017-10-10 23:10:38 -0500

    MVP

---
+++ family_hash.rb
@@ -0,0 +1,21 @@
+my_family = {
+    :sister => {
+        :name => 'Krista',
+        :age => 42
+    },
+    :mother => {
+        :name => 'Cathie',
+        :age => 70
+    }
+}
+
+
+
+
+sister = my_family[:sister]
+puts "My sister is #{sister[:name]} and is #{sister[:age]} years old."
+
+mother = my_family[:mother]
+puts "My mother is #{mother[:name]} and is #{mother[:age]} years old."
+
+
